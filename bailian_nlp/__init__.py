967442cc2b952c9a0dc6b7bf12d9e147f50053f9
liuxiang <liuxiang@bailian.ai>
2019-04-11 20:55:19 +0800

    阶段性备份

--- bailian_nlp/__init__.py
+++ bailian_nlp/__init__.py
@@ -1,7 +1,7 @@
 from .modules.models.bert_models import BertBiLSTMCRF
 from .modules.train.train import NerLearner
 from .modules.data.bert_data import BertNerData
-from .released.models import PosTagger
+from .released.pos import PosTagger
 
 
 __all__ = ["NerLearner", "BertNerData", "BertBiLSTMCRF", "PosTagger"]
