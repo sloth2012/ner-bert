d3c259c1ae7d734d69bae051cb5dbfcd2e4e3c40
liuxiang <liuxiang@bailian.ai>
2019-04-28 11:58:06 +0800

    可以使用ernie

--- bailian_nlp/released/settings.py
+++ bailian_nlp/released/settings.py
@@ -7,14 +7,20 @@ for i in range(1):
     root_dir = os.path.dirname(root_dir)
 
 MODEL_DIR = os.path.join(root_dir, 'datadrive/models')
-CHINESE_MODEL_DIR = os.path.join(MODEL_DIR, 'chinese_L-12_H-768_A-12')
+
+
+USE_ERNIE = True
+model_dir_name = 'chinese_L-12_H-768_A-12' if not USE_ERNIE else 'ernie'
+model_name = 'bert' if not USE_ERNIE else 'ernie'
+
+CHINESE_MODEL_DIR = os.path.join(MODEL_DIR, model_dir_name)
 
 # 先主要处理中文的
 DEFAULT_POS_MODEL_CONFIG_FILE = os.path.join(CHINESE_MODEL_DIR, 'pos.json')
 DEFAULT_POS_MODEL_FILE = os.path.join(CHINESE_MODEL_DIR, 'pos.bin')
 
-CHINESE_BERT_MODEL_FILE = os.path.join(CHINESE_MODEL_DIR, 'bert_model.bin')
-CHINESE_BERT_MODEL_CONFIG_FILE = os.path.join(CHINESE_MODEL_DIR, 'bert_config.json')
+CHINESE_BERT_MODEL_FILE = os.path.join(CHINESE_MODEL_DIR, f'{model_name}_model.bin')
+CHINESE_BERT_MODEL_CONFIG_FILE = os.path.join(CHINESE_MODEL_DIR, f'{model_name}_config.json')
 CHINESE_BERT_VOCAB_FILE = os.path.join(CHINESE_MODEL_DIR, 'vocab.txt')
 
 POS_DATA_DIR = os.path.join(root_dir, 'datadrive/bailian')
