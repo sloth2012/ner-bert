e53dd41d8aff52f30f93b19cf05b25bf380d4502
liuxiang <liuxiang@bailian.ai>
2019-04-28 16:36:40 +0800

    多进程

--- bailian_nlp/web/server.py
+++ bailian_nlp/web/server.py
@@ -8,10 +8,13 @@ import urllib3
 from tornado.options import options, define
 import tornado.process
 
+
 define("port", default=50010, help="run on th given port", type=int)
 
 
 def main():
+    import torch.multiprocessing as mp
+    mp.set_start_method('spawn')
     from bailian_nlp.web.utils.logger import getLogger, LogFormatter
     from bailian_nlp.web.application import application
     logger = getLogger('server')
@@ -27,7 +30,7 @@ def main():
     [i.setFormatter(LogFormatter()) for i in logging.getLogger().handlers]
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.bind(options.port)
-    http_server.start(num_processes=0)
+    http_server.start(num_processes=2)
 
     logger.info('Development server is running at http://127.0.0.1:%s/' % options.port)
     logger.info('Quit the server with Control-C')
