74229f866e57163d9ebbdc7cb40b28a66e14a85a
liuxiang <liuxiang@bailian.ai>
2019-04-30 17:53:15 +0800

    增加一个探活接口

--- bailian_nlp/web/url.py
+++ bailian_nlp/web/url.py
@@ -1,8 +1,10 @@
 # coding: utf8
 
 from .handler.pos import PosTaggerHandler
+from .handler.ping import PingHandler
 
 url = [
     # 词性标注
-    (r'/pos', PosTaggerHandler)
+    (r'/pos', PosTaggerHandler),
+    (r'/_ping', PingHandler)
 ]
